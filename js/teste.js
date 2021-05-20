7b59e266a86e533bb476615ab9297e9c92806978
adorocode <rychell@maquiadoro.com.br>
2021-05-20 17:15:19 -0300

    Teste desenvolvedor web

---
+++ js/teste.js
@@ -0,0 +1,160 @@
+// Manipulação de dados e sintaxe
+const categories = [
+    {
+      id: 1,
+      name: "Sedan",
+      cars: [
+        {
+          id: 1,
+          name: "Onix Plus",
+          manufacturedYear: "2021",
+          color: "White",
+          price: 51990,
+          reviewRate: 4.8
+        },
+        {
+          id: 2,
+          name: "Fiat Cronos",
+          manufacturedYear: "2019",
+          color: "Black",
+          price: 61790,
+          reviewRate: 3.7
+        },
+        {
+          id: 3,
+          name: "Ford Ka Plus",
+          manufacturedYear: "2018",
+          color: "Red",
+          price: 49500,
+          reviewRate: 4.9
+        },
+        {
+          id: 4,
+          name: "Honda City",
+          manufacturedYear: "2015",
+          color: "Black",
+          price: 38790,
+          reviewRate: 4.1
+        },
+        {
+          id: 5,
+          name: "Renault Logan",
+          manufacturedYear: "2020",
+          color: "Silver",
+          price: 61790,
+          reviewRate: 4.15
+        },
+      ],
+    },
+    {
+      id: 2,
+      name: "Hatch",
+      cars: [
+        {
+          id: 6,
+          name: "Volkswagen Polo",
+          manufacturedYear: "2001",
+          color: "White",
+          price: 51990,
+          reviewRate: 4
+        },
+        {
+          id: 7,
+          name: "Fiat Argo",
+          manufacturedYear: "2009",
+          color: "Black",
+          price: 61790,
+          reviewRate: 3.5
+        },
+        {
+          id: 8,
+          name: "Toyota Yaris",
+          manufacturedYear: "2008",
+          color: "White",
+          price: 49500,
+          reviewRate: 5
+        },
+        {
+          id: 9,
+          name: "Renault Sandero",
+          manufacturedYear: "2005",
+          color: "Silver",
+          price: 38790,
+          reviewRate: 4.7
+        },
+        {
+          id: 10,
+          name: "Peugeot 208",
+          manufacturedYear: "2000",
+          color: "Blue",
+          price: 61790,
+          reviewRate: 4.68
+        },
+      ],
+    },
+    {
+      id: 3,
+      name: "SUV",
+      cars: [
+        {
+          id: 11,
+          name: "Renault Duster",
+          manufacturedYear: "2011",
+          color: "White",
+          price: 30990,
+          reviewRate: 4.66
+        },
+        {
+          id: 12,
+          name: "Ford Ecosport",
+          manufacturedYear: "2019",
+          color: "Black",
+          price: 61790,
+          reviewRate: 2.4
+        },
+        {
+          id: 13,
+          name: "Nissan Kicks",
+          manufacturedYear: "2018",
+          color: "White",
+          price: 35500,
+          reviewRate: 3.98
+        },
+        {
+          id: 14,
+          name: "Hyundai Creta",
+          manufacturedYear: "2015",
+          color: "Silver",
+          price: 48790,
+          reviewRate: 4.26
+        },
+        {
+          id: 15,
+          name: "Jeep Renegade",
+          manufacturedYear: "2010",
+          color: "Blue",
+          price: 28790,
+          reviewRate: 3.5
+        },
+      ],
+    },
+  ];
+  
+  /* Perguntas
+  1 - Crie uma função que receba o objeto acima e retorne um array com o nome do carro mais antigo de cada categoria.
+  2 - Crie uma função que receba o objeto acima e retorne a média das avaliações dos carros que fazem parte da categoria
+  3 - Crie uma função que receba o objeto acima e retorne um array com o nome de cada carro  
+  */
+  
+  // Manipulação de DOM
+  
+  /**
+  1 - 
+   
+   */
+  
+  // Fetch
+  /**
+   * 
+   */
+  
